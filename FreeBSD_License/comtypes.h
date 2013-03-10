d4c0b995f95f4f07caf886edc009dca585830fff
pidh <pigh@github.com>
2013-03-10 16:58:46 +0900

    hmac_sha256 is added

--- FreeBSD_License/comtypes.h
+++ FreeBSD_License/comtypes.h
@@ -22,12 +22,13 @@
 ** OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ** ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#ifndef	__SHA1_H__
-#define	__SHA1_H__
+#ifndef	__COMTYPES_H__
+#define	__COMTYPES_H__
 
-void SHA1_Calc(void *_ctx, u1 *b, u4 len);
-void *SHA1_Init(void);
-void SHA1_Finish(void *_ctx, u1 *digest);
+/*>>Platform depends part*/
+typedef	unsigned int	u4;
+typedef	unsigned char	u1;
+/*<<Platform depends part*/
 
 #endif
 
--- FreeBSD_License/hmac_sha256.c
+++ FreeBSD_License/hmac_sha256.c
@@ -1,5 +1,5 @@
 /*
-** Copyright 2011-2013 pidh <github360@yahoo.co.jp>. All rights reserved.
+** Copyright 2013 pidh <github360@yahoo.co.jp>. All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
 ** modification, are permitted provided that the following conditions are met:
@@ -22,12 +22,51 @@
 ** OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 ** ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#ifndef	__SHA1_H__
-#define	__SHA1_H__
+/*>>Platform depends part*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
 
-void SHA1_Calc(void *_ctx, u1 *b, u4 len);
-void *SHA1_Init(void);
-void SHA1_Finish(void *_ctx, u1 *digest);
+#define	MEMCPY	memcpy
+#define	MEMSET	memset
+#define	MALLOC	malloc
+#define	FREE	free
 
-#endif
+/*<<Platform depends part*/
+#include "comtypes.h"
+#include "sha256.h"
 
+
+void
+HMAC_SHA256_Calc(u1 *text, int text_len, u1 *key, int key_len, u1 *digest)
+{
+	u1	tmp[32];	/* L=32 */
+	u1	pad[64];
+	void	*ctx;
+	int	i;
+
+	if(key_len > 64) {
+		ctx = SHA256_Init();
+		SHA256_Calc(ctx, key, key_len);
+		SHA256_Finish(ctx, tmp);
+		key = tmp;
+		key_len = sizeof(tmp);
+	}
+
+	MEMSET(pad, 0x36, sizeof(pad));
+	for(i = 0;i < key_len;i++)
+		pad[i] ^= key[i];
+
+	ctx = SHA256_Init();
+	SHA256_Calc(ctx, pad, sizeof(pad));
+	SHA256_Calc(ctx, text, text_len);
+	SHA256_Finish(ctx, digest);
+
+	for(i = 0;i < sizeof(pad);i++)
+		pad[i] ^= 0x5c ^ 0x36;
+
+	ctx = SHA256_Init();
+	SHA256_Calc(ctx, pad, sizeof(pad));
+	SHA256_Calc(ctx, digest, 32);
+	SHA256_Finish(ctx, digest);
+}
